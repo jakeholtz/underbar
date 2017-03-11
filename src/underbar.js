9ede4a438206fee5231d9e1fcfb1ab57cf35bc90
Jake Holtz <jahholtz@gmail.com>
2017-03-10 21:03:54 -0800

    ALL FIXED underbar.js PART I & PART II

--- src/underbar.js
+++ src/underbar.js
@@ -115,10 +115,8 @@
     return accumulator;
   };
 
-  // Determine if the array or object contains a given value (using `===`).
+
   _.contains = function(collection, target) {
-    // TIP: Many iteration problems can be most easily expressed in
-    // terms of reduce(). Here's a freebie to demonstrate!
     return _.reduce(collection, function(wasFound, item) {
       if (wasFound) {
         return true;
@@ -128,7 +126,6 @@
   };
 
 
-  // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     return _.reduce(collection, function(allPass, item) {
       if (!allPass) {
@@ -138,10 +135,8 @@
     }, true);
   };
 
-  // Determine whether any of the elements pass a truth test. If no iterator is
-  // provided, provide a default one
+
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
     return _.reduce(collection, function(onePass, item) {
       if (onePass) {
         return true;
@@ -151,24 +146,6 @@
   };
 
 
-  /**
-   * OBJECTS
-   * =======
-   *
-   * In this section, we'll look at a couple of helpers for merging objects.
-   */
-
-  // Extend a given object with all the properties of the passed in
-  // object(s).
-  //
-  // Example:
-  //   var obj1 = {key1: "something"};
-  //   _.extend(obj1, {
-  //     key2: "something new",
-  //     key3: "something else new"
-  //   }, {
-  //     bla: "even more stuff"
-  //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(object) {
     for (var i = 1; i < arguments.length; i++) {
       _.each(arguments[i], function(value, prop) {
@@ -178,8 +155,7 @@
     return object;
   };
 
-  // Like extend, but doesn't ever overwrite a key that already
-  // exists in obj
+
   _.defaults = function(object) {
     for (var i = 1; i < arguments.length; i++) {
       _.each(arguments[i], function(value, prop) {
@@ -190,45 +166,17 @@
   };
 
 
-  /**
-   * FUNCTIONS
-   * =========
-   *
-   * Now we're getting into function decorators, which take in any function
-   * and return out a new version of the function that works somewhat differently
-   */
-
-  // Return a function that can be called at most one time. Subsequent calls
-  // should return the previously returned value.
   _.once = function(func) {
-    // TIP: These variables are stored in a "closure scope" (worth researching),
-    // so that they'll remain available to the newly-generated function every
-    // time it's called.
     var alreadyCalled = false;
     var result;
-
-    // TIP: We'll return a new function that delegates to the old one, but only
-    // if it hasn't been called before.
     return function() {
       if (!alreadyCalled) {
-        // TIP: .apply(this, arguments) is the standard way to pass on all of the
-        // infromation from one function call to another.
         result = func.apply(this, arguments);
         alreadyCalled = true;
       }
-      // The new function always returns the originally computed result.
       return result;
     };
   };
-
-  // Memorize an expensive function's results by storing them. You may assume
-  // that the function only takes primitives as arguments.
-  // memoize could be renamed to oncePerUniqueArgumentList; memoize does the
-  // same thing as once, but based on many sets of unique arguments.
-  //
-  // _.memoize should return a function that, when called, will check if it has
-  // already computed the result for the given argument and return that value
-  // instead if possible.
 
 
 _.memoize = function(func) {
@@ -247,27 +195,12 @@ _.memoize = function(func) {
    };
  };
 
-  // Delays a function for the given number of milliseconds, and then calls
-  // it with the arguments supplied.
-  //
-  // The arguments for the original function are passed after the wait
-  // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
-  // call someFunction('a', 'b') after 500ms
+
   _.delay = function(func, wait) {
     setTimeout(arguments[0], arguments[1], arguments[2], arguments[3]);
   };
 
 
-  /**
-   * ADVANCED COLLECTION OPERATIONS
-   * ==============================
-   */
-
-  // Randomizes the order of an array's contents.
-  //
-  // TIP: This function's test suite will ask that you not modify the original
-  // input array. For a tip on how to make a copy of an array, see:
-  // http://mdn.io/Array.prototype.slice
   _.shuffle = function(array) {
     var output = array.slice(0);
     output.unshift(output.pop());
