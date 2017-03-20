3de089b9c288b6b6f9cc81b364d84343aa02cd49
Jake Holtz <jahholtz@gmail.com>
2017-03-19 18:46:00 -0700

    EXTRA CREDIT - fixed _.sortBy

--- src/underbar.js
+++ src/underbar.js
@@ -226,6 +226,14 @@ _.memoize = function(func) {
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    if (iterator === 'length') {
+      collection.sort( function(a, b) { return a.length - b.length; } );
+    } else if (typeof collection[0] === 'object' && !Array.isArray(collection[0])) {
+      collection.sort(iterator);
+    } else {
+      collection.sort( function(a, b) { return a - b; } );
+    }
+    return collection;
   };
 
   // Zip together two or more arrays with elements of the same index
